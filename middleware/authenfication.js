c737e7b82e21b3ee2718f65b91165d0a0894de9b
hsh411 <elitebook855@gmail.com>
2022-01-18 16:42:05 +0900

    라우터 분리

--- middleware/authenfication.js
+++ middleware/authenfication.js
@@ -1,10 +1,10 @@
 /* 소셜 로그인 체크하는 미들웨어 */
-
 const authentication = (req, res, next) => {
   const { session } = req;
   if (session.auth_data === undefined) {
-    res.redirect('/');
+    res.status(302).redirect('/?msg=유효하지 않은 사용자');
   } else {
+    console.log('로그인 성공');
     next();
   }
 };
--- routers/social.js
+++ routers/social.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { authentication } = require('../middleware/authenfication');
+const {
+  kakao_check,
+  kakao_login,
+  kakao_logout,
+  kakao_profile,
+  kakao_unlink,
+} = require('./controllers/Oauth.social.login');
+
 /* 카카오 라우터 */
+router.get('/kakao', kakao_check);
+router.get('/kakao/login', kakao_login);
+router.get('/kakao/account_info', authentication, kakao_profile);
+router.get('/kakao/logout', authentication, kakao_logout);
+router.get('/kakao/unlink', authentication, kakao_unlink);
+
+module.exports = router;
